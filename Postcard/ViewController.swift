538746cb50a1688c2cbd1ed1c7e6e9cf9632b6f4
Doug Washburn <douglaswashburn@gmail.com>
2014-09-11 11:23:16 -0400

    first commit

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblDisplaySentMessage: UILabel!
+    
+    @IBOutlet weak var txtEnterName: UITextField!
+    
+    @IBOutlet weak var txtEnterMessage: UITextField!
+    
+    @IBOutlet weak var btnSendMail: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnSendMailPressed(sender: UIButton) {
+        // Code will evaluate when the button is pressed.
+        
+        // Unhide the label
+        lblDisplaySentMessage.hidden = false
+        
+        // set the label text to the text entered for the message
+        lblDisplaySentMessage.text = txtEnterMessage.text
+        
+        // Blank out the message field
+        txtEnterMessage.text = ""
+        
+        // Get rid of the keyboard
+        txtEnterMessage.resignFirstResponder()
+        
+        // Change label color
+        lblDisplaySentMessage.textColor=UIColor.redColor()
+        
+        // Change button text
+        btnSendMail.setTitle("Mail Sent", forState: UIControlState.Normal)
+        
+        
+    }
 
 }
 
